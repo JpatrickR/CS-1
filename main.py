4bb92b53f5057c3d39a9b1cc1ea3b70938f57df4
JpatrickR <patrickramirez886@gmail.com>
2021-03-23 14:49:02 +0800

    Compute for the sum

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# Compute for the sum
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+
+def sum(x, y):
+    return x+y
+
+
+print("The sum is: ", sum(num1, num2))
